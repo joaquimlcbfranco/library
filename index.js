8bc3e5653f29ab4299e8bc63df8666efa2edaf9f
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-02-13 03:31:55 +0000

    Validate image url with regex, set default background if empty

--- index.js
+++ index.js
@@ -31,9 +31,15 @@ function displayBook(id) {
 	card.appendChild(cardImage);
 	cardImage.classList.add('card-image');
 	cardImage.setAttribute('data-id', id);
-	if (myLibrary[findLibraryIndex(id)].link != '') {
+	const re1 = /(http){0,1}s{0,1}(:\/\/){0,1}(www.){0,1}.*\.{1}.*/;
+	const re2 = /\.{0,1}\/{1}(.*\/)*.*\.{1}/;
+	if (myLibrary[findLibraryIndex(id)].link == '') {
+		cardImage.style.backdropImag = 'linear-gradient(to right bottom,rgb(58, 77, 57) 0%,rgb(58, 77, 57) 50%,rgb(79, 111, 82) 50%,rgb(79, 111, 82) 100%)';
+	}
+	else {
 		cardImage.style.backgroundImage = `url(${myLibrary[findLibraryIndex(id)].link})`;
 	}
+	
 	const cardButtons = document.createElement('div');
 	cardImage.appendChild(cardButtons);
 	cardButtons.classList.add('card-buttons');
@@ -44,6 +50,7 @@ function displayBook(id) {
 	else {
 		const eyeSvg = '<svg class="hide" data-type="mark-read"' + ` data-id=${id} ` + 'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>"Mark as read"</title><path d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" /></svg>';
 		cardButtons.innerHTML = eyeSvg;
+		cardImage.classList.add('notread');
 	}
 	const pencilSvg = '<svg class="hide" data-type="edit"' + ` data-id=${id} ` + 'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>"Edit"</title><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"/></svg>';
 	const deleteSvg = '<svg class="hide" data-type="delete"' + ` data-id=${id} ` + 'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>"Delete"</title><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>';
@@ -89,12 +96,20 @@ form.addEventListener('submit', (e) => {
 	const formLink = document.querySelector('#image').value;
 	const formRead = document.querySelector('#read').checked;
 
-	addBookToLibrary(bookId, formTitle, formAuthor, formPages, formLink, formRead);
-	displayBook(bookId);
-
-	e.target.reset();
-	document.querySelector('#name').focus();
-	bookId++;
+	const re1 = /(http){0,1}s{0,1}(:\/\/){0,1}(www.){0,1}.*\.{1}.*/;
+	const re2 = /\.{0,1}\/{1}(.*\/)*.*\.{1}/;
+	if (re1.test(formLink) || re2.test(formLink) || !formLink) {
+		addBookToLibrary(bookId, formTitle, formAuthor, formPages, formLink, formRead);
+		displayBook(bookId);
+	
+		e.target.reset();
+		document.querySelector('#name').focus();
+		bookId++;
+	}
+	else {
+		alert('Please provide a valid URL');
+		return 1;
+	}
 });
 
 // Finds book with a certain index in the array with key == argument "id"
@@ -155,20 +170,33 @@ function editBook(id) {
 // When form is submitted, update array values and DOM element's text content
 dialogForm.addEventListener('submit', (e) => {
 	e.preventDefault();
+	const re3 = /(http){0,1}s{0,1}(:\/\/){0,1}(www.){0,1}.*\.{1}.*/;
 
 	const currentCardIndex = +dialogForm.getAttribute('data-id')
 	myLibrary[findLibraryIndex(currentCardIndex)].title = dialogTitle.value;
 	myLibrary[findLibraryIndex(currentCardIndex)].author = dialogAuthor.value;
 	myLibrary[findLibraryIndex(currentCardIndex)].pages = +dialogPages.value;
-	myLibrary[findLibraryIndex(currentCardIndex)].link = dialogLink.value;
-
+	if (re3.test(dialogLink.value) || !dialogLink.value) {
+		myLibrary[findLibraryIndex(currentCardIndex)].link = dialogLink.value;
+	}
+	else {
+		alert('Please provide a valid image URL.');
+	}
+	
 	const cardHeader = document.querySelector(`.card-details[data-id="${currentCardIndex}"] h4`);
 	const cardAuthor = document.querySelector(`.card-details[data-id="${currentCardIndex}"] p:nth-child(2)`);
 	const cardPages = document.querySelector(`.card-details[data-id="${currentCardIndex}"] p:nth-child(3)`);
+	const cardLink = document.querySelector(`.card-image[data-id="${currentCardIndex}"`);
 
 	cardHeader.textContent = dialogTitle.value;
 	cardAuthor.textContent = dialogAuthor.value;
 	cardPages.textContent = +dialogPages.value;
+	if (re3.test(dialogLink.value)) {
+		cardLink.style.backgroundImage = `url(${dialogLink.value})`;
+	}
+	else if (!dialogLink.value) {
+		cardLink.style.background = 'linear-gradient(to right bottom,rgb(58, 77, 57) 0%,rgb(58, 77, 57) 50%,rgb(79, 111, 82) 50%,rgb(79, 111, 82) 100%)';
+	}
 
 	dialog.close();
 });
@@ -189,15 +217,18 @@ function deleteBook(id) {
 // If true, changes svg innerHTML to non read one, sets svg attribute to the opposite and updates object's read status in array
 function readStatus(id) {
 	currentSvg = document.querySelector(`.card-buttons[data-id="${id}"] svg:nth-child(1)`);
+	currentParent = document.querySelector(`.card-image[data-id="${id}"]`)
 	if (myLibrary[findLibraryIndex(id)].read) {
 		currentSvg.innerHTML = '<title>"Mark as read"</title><path d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" />';
 		currentSvg.setAttribute('data-type', 'mark-read');
 		myLibrary[findLibraryIndex(id)].read = false;
+		currentParent.classList.add('notread');
 	}
 	else if (!myLibrary[findLibraryIndex(id)].read) {
 		currentSvg.innerHTML = '<title>"Mark as not read"</title><path d="M11.83,9L15,12.16C15,12.11 15,12.05 15,12A3,3 0 0,0 12,9C11.94,9 11.89,9 11.83,9M7.53,9.8L9.08,11.35C9.03,11.56 9,11.77 9,12A3,3 0 0,0 12,15C12.22,15 12.44,14.97 12.65,14.92L14.2,16.47C13.53,16.8 12.79,17 12,17A5,5 0 0,1 7,12C7,11.21 7.2,10.47 7.53,9.8M2,4.27L4.28,6.55L4.73,7C3.08,8.3 1.78,10 1,12C2.73,16.39 7,19.5 12,19.5C13.55,19.5 15.03,19.2 16.38,18.66L16.81,19.08L19.73,22L21,20.73L3.27,3M12,7A5,5 0 0,1 17,12C17,12.64 16.87,13.26 16.64,13.82L19.57,16.75C21.07,15.5 22.27,13.86 23,12C21.27,7.61 17,4.5 12,4.5C10.6,4.5 9.26,4.75 8,5.2L10.17,7.35C10.74,7.13 11.35,7 12,7Z" />';
 		currentSvg.setAttribute('data-type', 'mark-unread');
 		myLibrary[findLibraryIndex(id)].read = true;
+		currentParent.classList.remove('notread');
 	}
 }
 
